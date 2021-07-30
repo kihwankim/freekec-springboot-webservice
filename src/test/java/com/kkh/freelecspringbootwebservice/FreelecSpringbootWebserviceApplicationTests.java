187a51cdc4c7e75f6c1f9020fc45a4e496cdc846
kihwankim <clack2933@gmail.com>
2021-07-30 22:37:14 +0900

    feat: init

---
+++ src/test/java/com/kkh/freelecspringbootwebservice/FreelecSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkh.freelecspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
