4c2833e3305302e5c37af67bd717006e38458095
kihwankim <clack2933@gmail.com>
2021-08-06 22:31:15 +0900

    feat: 글 등록 페이지 제작

--- src/main/java/com/kkh/freelecspringbootwebservice/web/IndexController.java
+++ src/main/java/com/kkh/freelecspringbootwebservice/web/IndexController.java
@@ -1,12 +1,19 @@
 package com.kkh.freelecspringbootwebservice.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
