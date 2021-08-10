9db13159dfe756108e657e36d4342889dda64c8f
kihwankim <clack2933@gmail.com>
2021-08-10 23:04:30 +0900

    feat: user role 도메인 추가

---
+++ src/main/java/com/kkh/freelecspringbootwebservice/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.kkh.freelecspringbootwebservice.domain.user;
+
+import com.kkh.freelecspringbootwebservice.domain.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
