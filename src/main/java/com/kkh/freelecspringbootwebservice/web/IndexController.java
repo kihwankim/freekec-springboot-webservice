6dbc58c598ee0dfd7d51253293a792ebbb1d3c63
kihwankim <clack2933@gmail.com>
2021-08-11 21:47:14 +0900

    feat: oauth fron end 로그인 연결 코드 추가

--- src/main/java/com/kkh/freelecspringbootwebservice/web/IndexController.java
+++ src/main/java/com/kkh/freelecspringbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kkh.freelecspringbootwebservice.web;
 
+import com.kkh.freelecspringbootwebservice.config.auth.dto.SessionUser;
 import com.kkh.freelecspringbootwebservice.service.posts.PostService;
 import com.kkh.freelecspringbootwebservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
