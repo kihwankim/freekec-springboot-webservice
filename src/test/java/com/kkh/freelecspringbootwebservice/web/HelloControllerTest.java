8af18eae5fba1b2ca30bade96e50ae1823557f3f
kihwankim <clack2933@gmail.com>
2021-08-12 20:31:23 +0900

    test: mock mvc로 security해서 테스트 코드 변경

--- src/test/java/com/kkh/freelecspringbootwebservice/web/HelloControllerTest.java
+++ src/test/java/com/kkh/freelecspringbootwebservice/web/HelloControllerTest.java
@@ -1,12 +1,17 @@
 package com.kkh.freelecspringbootwebservice.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,9 +20,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class HelloControllerTest {
 
     @Autowired
+    WebApplicationContext webApplicationContext;
+
     MockMvc mvc;
 
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
+    @WithMockUser(roles = "USER")
     void hello가_리턴된다() throws Exception {
         // given
 
@@ -31,6 +47,7 @@ class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     void helloDto가_리턴된다() throws Exception {
         // given
         String name = "hello";
